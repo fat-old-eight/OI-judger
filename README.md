df5a4fd5dfeb222b3ea3028e1e7fb6fdce98bc14
FatOldEight <tzq80820@163.com>
2023-10-17 21:26:09 +0800

    update

--- README.md
+++ README.md
@@ -7,12 +7,14 @@
 會自動編譯 SPJ 和 Source 文件。
 ```
 Usage:
-./judge.sh [-t TimeLimit] [-c Checker] [-s Source] [-g]
+Usage:
+./judge.sh [-t TimeLimit] [-c Checker] [-s Source] [-g] [-f Freopen file]
 Description:
-TimeLimit,unit second.
-Checker,the cpp or binary file of special judge.
-Source,the cpp you want to run.
--g,generate debugging information at compile time.
+TimeLimit, unit second.
+Checker, the cpp or binary file of special judge.
+Source, the cpp you want to run.
+-g, generate debugging information at compile time.
+Freopen file, the name of in/out/ans name(if ues freopen).
 ```
 ## 注意
 現在不支持 subtest 和判斷 MLE。
@@ -20,3 +22,5 @@ Source,the cpp you want to run.
 以及如果文件夾下有多個 cpp 文件，請指定 Source。
 
 歡迎 PR。（如果不介意如答辯一樣的代碼的話。）
+
+RE 有些許排版問題，正在修補。
--- judge.sh
+++ judge.sh
@@ -5,6 +5,7 @@ ulimit -s unlimited
 op="-std=c++14 -O2"
 gdbop="-std=c++14 -Og -g"
 spjop="-std=c++14 -O2"
+dir=".judgerdir"
 #########################
 TimeLimit=1
 SPJ="./"
@@ -13,29 +14,62 @@ WA=()
 TLE=()
 CPP="4747732939492939492394948384923949294"
 gdb=0
+fre=0
+rmdir=1
+ffile=""
 #########################
 
 func() {
     echo "Usage:"
-    echo "./judge.sh [-t TimeLimit] [-c Checker] [-s Source] [-g]"
+    echo "./judge.sh [-t TimeLimit] [-c Checker] [-s Source] [-g] [-f Freopen file]"
     echo "Description:"
-    echo "TimeLimit,unit second."
-    echo "Checker,the cpp or binary file of special judge."
-    echo "Source,the cpp you want to run."
-    echo "-g,generate debugging information at compile time."
-    exit -1
+    echo "TimeLimit, unit second."
+    echo "Checker, the cpp or binary file of special judge."
+    echo "Source, the cpp you want to run."
+    echo "-g, generate debugging information at compile time."
+    echo 'Freopen file, the name of in/out/ans name(if ues freopen).'
+    exit 0
 }
 
-while getopts 'ht:c:s:g' OPT; do
+while getopts 'ht:c:s:gf:' OPT; do
     case $OPT in
         t) TimeLimit="$OPTARG";;
         c) SPJ="$OPTARG";;
         s) CPP="$OPTARG";;
         g) gdb=1;;
+        f) fre=1;ffile="$OPTARG";;
         h) func;;
         ?) func;;
     esac
 done
+########function#########
+check(){
+    if [[ $ret == 124 ]];then
+        echo -n "$(tput sgr0)TLE"
+        TLE=("${TLE[@]}" $4)
+    elif [[ $ret != 0 ]];then
+        echo -n "$(tput setaf 5)RE"
+        RE=("${RE[@]}" $4)
+    else
+        if [ $SPJ != "./" ];then
+            "./$SPJ" $1 $2 $3
+            if [[ $? != 0 ]];then
+                WA=("${WA[@]}" $4)
+            fi
+        else
+            diff -ZB $2 $3 > /dev/null
+            if [[ $? != 0 ]];then
+                echo -n "$(tput setaf 1)WA"
+                WA=("${WA[@]}" $4)
+            else
+                echo -n "$(tput setaf 2)AC"
+            fi
+        fi
+    fi
+    echo $(tput sgr0)
+    echo "Spend Time: $Time s"
+    echo
+}
 #########################
 a=$(ls *.cpp 2>/dev/null)
 cnt=0
@@ -48,10 +82,10 @@ do
     fi
 done
 if [[ $cnt -ge 2 && $CPP == "4747732939492939492394948384923949294" ]];then
-    echo "too more cpp file here,please use -s."
+    echo "too more cpp file here, please use -s."
     exit -1
 fi
-if [[ $cnt -eq 0 ]];then
+if [[ $cnt == 0 ]];then
     echo "no cpp file here."
     exit -1
 fi
@@ -62,7 +96,7 @@ fi
 if [ ! -e $SPJ ];then
     if [ -e "$SPJ.cpp" ];then
         g++ "$SPJ.cpp" -o $SPJ $spjop 2>/dev/null
-        if [ $? -ne 0 ];then
+        if [[ $? != 0 ]];then
             echo "$(tput setaf 11)special judge CE."
             exit -1
         fi
@@ -75,12 +109,12 @@ if [ $CPP != "4747732939492939492394948384923949294" ];then
     cpp=$CPP
 fi
 tmp=${cpp:0:$(expr ${#cpp} - 4)}
-if [ $gdb -ne 1 ];then
+if [[ $gdb != 1 ]];then
     g++ $cpp -o $tmp $op 2>/dev/null
 else
     g++ $cpp -o $tmp $gdbop 2>/dev/null
 fi
-if [ $? -ne 0 ];then
+if [[ $? != 0 ]];then
     echo  "$(tput setaf 11)CE"
     exit -1
 fi
@@ -90,17 +124,26 @@ for in in $fl
 do
     cnt=$(expr $cnt + 1);
 done
-if [ $cnt -eq 0 ];then
+if [[ $cnt == 0 ]];then
     echo "no in/out/ans file here."
     exit -1
 fi
 i=1
+if [[ $fre == 1 ]];then
+    if [[ -d $dir ]];then
+        rmdir=0
+    else
+        mkdir $dir
+    fi
+    cp $tmp "$dir/$tmp"
+fi
 for in in $fl
 do
     out=$(echo $in | sed "s/.in$/.out/")
     ans=$(echo $in | sed "s/.in$/.ans/")
     name=${in:0:$(expr ${#in} - 3)}
     echo "$name[$i/$cnt]:"
+    i=$(expr $i + 1)
     if [ ! -e $ans ]
     then
         if [ ! -e $out ];then
@@ -117,35 +160,28 @@ do
             mv $ans1 $ans
         fi
     fi
-    Time=$( TIMEFORMAT="%R"; time ( timeout $TimeLimit ./$tmp <$in > $out ) 2>&1 )
-    ret=$?
-    if [ $ret -eq 124 ];then
-        echo -n "$(tput sgr0)TLE"
-        TLE=("${TLE[@]}" $name)
-    elif [ $ret -ne 0 ];then
-        echo -n "$(tput setaf 5)RE"
-        RE=("${RE[@]}" $name)
-    else
-        if [ $SPJ != "./" ];then
-            "./$SPJ" $in $out $ans
-            if [ $? -ne 0 ];then
-                WA=("${WA[@]}" $name)
-            fi
-        else
-            diff -ZB $ans $out > /dev/null
-            if [ $? -ne 0 ];then
-                echo -n "$(tput setaf 1)WA"
-                WA=("${WA[@]}" $name)
-            else
-                echo -n "$(tput setaf 2)AC"
-            fi
-        fi
+    if [[ $fre == 1 ]];then
+        cp $in "./$dir/$ffile.in"
+        cp $ans "./$dir/$ffile.ans"
+        cd $dir
+        Time=$( TIMEFORMAT="%R"; time ( timeout $TimeLimit ./$tmp 2>/dev/null) 2>&1 )
+        ret=$?
+        check "$ffile.in" "$ffile.out" "$ffile.ans" $name
+        cd ..
+        continue
     fi
-    echo $(tput sgr0)
-    echo "Spend Time: $Time s"
-    echo
-    i=$(expr $i + 1)
+    Time=$( TIMEFORMAT="%R"; time ( timeout $TimeLimit ./$tmp <$in >$out 2>/dev/null) 2>&1 )
+    ret=$?
+    check $in $out $ans $name
 done
+if [[ $fre -eq 1 && $rmdir -eq 1 ]];then
+    rm "./$dir/$ffile.in"
+    rm "./$dir/$ffile.out"
+    rm "./$dir/$ffile.ans"
+    if [ $rmdir -eq 1 ];then
+        rm $dir -r
+    fi
+fi
 echo "$(tput setaf 5)RE on: ${RE[@]}"
 echo "$(tput setaf 1)WA on: ${WA[@]}"
 echo "$(tput sgr0)TLE on: ${TLE[@]}"
